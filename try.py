733bcfc82869f72d6eadd75de61ab643832865f4
Bachelor <AndyWolfCF2013@163.com>
2020-06-16 22:06:32 +0800

    defeat try

--- try.py
+++ try.py
@@ -1,10 +1,11 @@
 import cv2 as cv 
+import matplotlib.pyplot as plt 
 
-img_L = cv2.imread("",0)
-img_R = cv2.imread("",0)
+img_L = cv.imread("IMG_20171206_143323.jpg",0)
+img_R = cv.imread("IMG_20171206_143323.jpg",0)
  
-stereo = cv2.StereoBM(cv2.STEREO_BM_BASIC_PRESET,16,15)
-# stereo = cv2.createStereoBM(numDisparities=16, blockSize=15)  OpenCV 3.0的函数
+#stereo = cv.StereoBM(cv.STEREO_BM_BASIC_PRESET,ndisparities=16, SADWindowSize=15)
+stereo = cv.createStereoBM(numDisparities=16, blockSize=15)  #OpenCV 3.0的函数
 disparity = stereo.compute(img_L, img_R)
  
 plt.subplot(121),plt.imshow(img_L,'gray'),plt.title('img_left'),plt.xticks([]),plt.yticks([])
